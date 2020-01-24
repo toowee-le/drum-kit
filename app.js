93f6bbcbca23402a155cf68653ff369d68f4f701
Tuyet Le <toowee.le@gmail.com>
2020-01-24 15:46:11 +0000

    Add property to return the audio playback time to the start with no delay

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if (!audio) {
-        return; // Stop the function from running all together
-    }
+    if (!audio) return; // Stop the function from running all together
+    audio.currentTime = 0; // Add property to return audio playback time to the start
     audio.play(); // Play method to play the audio
 });
